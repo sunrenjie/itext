6dd878e9ac8e32186fe2154c1822bafe6cca2ec7
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-01-16 11:24:25 +0000

    added 2001 to copyright notice

---
+++ www/tutorial/Chap0101.java
@@ -0,0 +1,66 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Chap0101 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Chapter 1 example 1: Hello World");
+
+		// step 1: creation of a document-object
+		Document document = new Document();
+
+		try {
+
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a PDF-stream to a file
+
+			PdfWriter.getInstance(document, new FileOutputStream("Chap0101.pdf"));
+
+			// step 3: we open the document
+			document.open();
+
+			// step 4: we add a paragraph to the document
+			document.add(new Paragraph("Hello World")); 
+
+		}
+		catch(DocumentException de) {
+			System.err.println(de.getMessage());
+		}
+		catch(IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+
+		// step 5: we close the document
+		document.close();
+	}
+}
