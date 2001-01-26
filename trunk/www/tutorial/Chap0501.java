92aa97692a4f4bc0838a64b881bbbcd90d52fb9a
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-01-26 13:16:53 +0000

    small changes

--- trunk/www/tutorial/Chap0501.java
+++ trunk/www/tutorial/Chap0501.java
@@ -26,30 +26,20 @@
 import java.awt.Color;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
 import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfWriter;
-
 public class Chap0501 {
-
 	public static void main(String[] args) {
-
 		System.out.println("Chapter 5 example 1: My first table");
-
 		// step 1: creation of a document-object
 		Document document = new Document();
-
 		try {
-
 			// step 2:
 			// we create a writer that listens to the document
 			// and directs a PDF-stream to a file
-
 			PdfWriter.getInstance(document, new FileOutputStream("Chap0501.pdf"));
-
 			// step 3: we open the document
 			document.open();
-
 			// step 4: we create a table and add it to the document
 			Table table = new Table(3);
 			table.setBorderWidth(1); 
@@ -83,7 +73,6 @@ public class Chap0501 {
 		catch(IOException ioe) {
 			System.err.println(ioe.getMessage());
 		}
-
 		// step 5: we close the document
 		document.close();
 	}
