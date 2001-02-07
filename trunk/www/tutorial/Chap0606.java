ec569f2be0c97c11311569025aae76319764cdf8
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-02-07 15:16:24 +0000

    finished chapter 6

---
+++ trunk/www/tutorial/Chap0606.java
@@ -0,0 +1,69 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Chap0606 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Chapter 6 example 6: Absolute Positioning of an Image");
+
+		// step 1: creation of a document-object
+		Document document = new Document();
+
+		try {
+
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a PDF-stream to a file
+
+			PdfWriter.getInstance(document, new FileOutputStream("Chap0606.pdf"));
+
+			// step 3: we open the document
+			document.open();
+
+			// step 4: we add content
+			Image png = Image.getInstance("hitchcock.png");
+			png.setAbsolutePosition(171, 250);
+			document.add(png); 
+			png.setAbsolutePosition(342, 500);
+			document.add(png);
+		}
+		catch(DocumentException de) {
+			System.err.println(de.getMessage());
+		}
+		catch(IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+
+		// step 5: we close the document
+		document.close();
+	}
+}
