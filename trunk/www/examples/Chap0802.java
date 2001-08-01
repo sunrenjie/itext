9a8c85da42ea7be9e786ac57f50256dfba1ccaa0
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-08-01 10:11:28 +0000

    changed structure tutorial

---
+++ trunk/www/examples/Chap0802.java
@@ -0,0 +1,70 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.xml.sax.Parser;
+import org.xml.sax.helpers.ParserFactory;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.html.HtmlWriter;
+import com.lowagie.text.xml.*;
+
+public class Chap0802 {
+
+    private static final String PARSER = "org.apache.xerces.parsers.SAXParser";
+        
+	public static void main(String[] args) {
+
+		System.out.println("Chapter 8 example 2: parsing example 1");
+
+		// step 1: creation of a document-object
+		Document document = new Document();
+
+		try {
+
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a XML-stream to a file
+ 			PdfWriter.getInstance(document, new FileOutputStream("Chap0802.pdf"));
+ 			HtmlWriter.getInstance(document, new FileOutputStream("Chap0802.html"));
+
+			// step 3: we create a parser and set the document handler
+            Parser parser = ParserFactory.makeParser(PARSER);
+            parser.setDocumentHandler(new SAXiTextHandler(document, new Tags4iText()));
+
+			// step 4: we parse the document
+            parser.parse("Chap0801.xml");
+            
+            
+		}
+		catch(Exception e) {
+            e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+	}
+}
