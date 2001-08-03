71f4efa7fc7cf696ff061fb44a6f7d4777789a51
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-08-03 08:46:28 +0000

    changed DTD

---
+++ trunk/www/examples/compile.bat
@@ -0,0 +1,2 @@
+javac -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/xerces_1_3_1.jar;. %1.java
+java -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/xerces_1_3_1.jar;. %1
