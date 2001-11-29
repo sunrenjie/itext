afaf0366a1d51a6c7f14c3d38a8aa22089537c43
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-11-29 14:10:41 +0000

    table example

---
+++ trunk/www/examples/Chap0502.java
@@ -0,0 +1,62 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.io.*;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+import com.lowagie.text.html.*;
+
+public class Chap0502 {
+    
+    public static void main(String[] args)
+    {
+        Document document = new Document(PageSize.A4, 50, 50, 50, 50);
+        try
+        {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Chap0502.pdf"));
+            
+            Font font8 = new Font(Font.HELVETICA, 8);
+            document.open();
+            Table table=new Table(1);
+            
+            Cell cell=new Cell();
+            cell.setLeading(font8.size());
+            for(int i=0;i<100;i++) {
+                cell.add(new Chunk(i+" of "+100+" dummy line\n",font8));
+            }
+            
+            
+            table.addCell(cell);
+            
+            document.add(table);
+            document.close();
+        }
+        catch (Exception de)
+        {
+            de.printStackTrace();
+        }
+    }
+}
