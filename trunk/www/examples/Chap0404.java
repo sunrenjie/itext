951c825fefc5e7ffe902c70409ce6a91e3834dee
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2001-12-13 15:47:21 +0000

    the Graphic object

---
+++ trunk/www/examples/Chap0404.java
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Chap0404 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Chapter 4 example 4: Simple Graphic");
+
+		// step 1: creation of a document-object
+		Document document = new Document();
+
+		try {
+
+			// step 2:
+			// we create a writer that listens to the document
+			// and directs a PDF-stream to a file
+
+			PdfWriter.getInstance(document, new FileOutputStream("Chap0404.pdf"));
+
+			// step 3: we open the document
+			document.open();
+
+			// step 4: we add a Graphic to the document
+			Graphic grx = new Graphic();
+			// add a rectangle
+			grx.rectangle(100, 700, 100, 100);
+			// add the diagonal
+			grx.moveTo(100, 700);
+			grx.lineTo(200, 800);
+			// stroke the lines
+			grx.stroke();
+			document.add(grx);
+
+		}
+		catch(DocumentException de) {
+			System.err.println(de.getMessage());
+		}
+		catch(IOException ioe) {
+			System.err.println(ioe.getMessage());
+		}
+
+		// step 5: we close the document
+		document.close();
+	}
+}
---
+++ trunk/www/examples/Chap0405.java
@@ -0,0 +1,76 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
+import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class Chap0405 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Chapter 4 example 5: page borders and horizontal lines");
+        
+        // step 1: creation of a document-object
+        Document document = new Document();
+        
+        try {
+            
+            // step 2: we create a writer that listens to the document
+            PdfWriter.getInstance(document, new FileOutputStream("Chap0405.pdf"));
+            
+            // step 3: we open the document
+            document.open();
+            
+            // step 4: we add a paragraph to the document
+            Graphic g = new Graphic();
+            g.setBorder(3f, 5f);
+            document.add(g);
+            document.add(new Paragraph("Hello World"));
+            document.add(new Paragraph("Hello World\n\n"));
+            g = new Graphic();
+            g.setHorizontalLine(5f, 100f);
+            document.add(g);
+            document.add(new Paragraph("Hello World"));
+            document.add(new Paragraph("Hello World\n\n"));
+            g = new Graphic();
+            g.setHorizontalLine(2f, 80f, new Color(0xFF, 0x00, 0x00));
+            document.add(g);
+            document.add(new Paragraph("Hello World"));
+            
+        }
+        catch(DocumentException de) {
+            System.err.println(de.getMessage());
+        }
+        catch(IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        
+        // step 5: we close the document
+        document.close();
+    }
+}
