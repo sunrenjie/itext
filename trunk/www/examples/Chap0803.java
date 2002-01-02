07d6a530288c8b7bdae9dd8b84e9e6b9a5945959
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-01-02 09:18:59 +0000

    Changes Mark Hall

--- trunk/www/examples/Chap0803.java
+++ trunk/www/examples/Chap0803.java
@@ -5,7 +5,7 @@
  * This code is free software. It may only be copied or modified
  * if you include the following copyright notice:
  *
- * --> Copyright 2000, 2001 by Bruno Lowagie <--
+ * --> Copyright 2001 by Bruno Lowagie <--
  *
  * This code is part of the 'iText Tutorial'.
  * You can find the complete tutorial at the following address:
@@ -23,49 +23,47 @@
  * bruno@lowagie.com
  */
 
-import java.io.*;
 import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import com.lowagie.text.*;
 import com.lowagie.text.rtf.RtfWriter;
 
 public class Chap0803 {
+
     public static void main(String[] args) {
-        System.out.println("Chapter 8 example 3: Tables and RTF");
+
+        System.out.println("Chapter 8 example 3: Lists and RTF");
+
         // step 1: creation of a document-object
         Document document = new Document();
+
         try {
+
             // step 2:
             // we create a writer that listens to the document
             // and directs a PDF-stream to a file
             RtfWriter.getInstance(document, new FileOutputStream("Chap0803.rtf"));
+
             // step 3: we open the document
             document.open();
-            // step 4: we create a table and add it to the document
-            Table table = new Table(3);
-            table.setBorderWidth(1);
-            table.setBorderColor(new Color(0, 0, 255));
-            table.setPadding(5);
-            table.setSpacing(5);
-            Cell cell = new Cell("header");
-            cell.setHeader(true);
-            cell.setColspan(3);
-            table.addCell(cell);
-            cell = new Cell("example cell with colspan 1 and rowspan 2");
-            cell.setRowspan(2);
-            cell.setBorderColor(new Color(255, 0, 0));
-            table.addCell(cell);
-            table.addCell("1.1");
-            table.addCell("2.1");
-            table.addCell("1.2");
-            table.addCell("2.2");
-            table.addCell("cell test1");
-            cell = new Cell("big cell");
-            cell.setRowspan(2);
-            cell.setColspan(2);
-            cell.setBackgroundColor(new Color(0xC0, 0xC0, 0xC0));
-            table.addCell(cell);
-            table.addCell("cell test2");
-            document.add(table);
+
+            // step 4:
+
+            document.add(new Paragraph("some books I really like:"));
+            ListItem listItem;
+            List list = new List(true, 15);
+            listItem = new ListItem("When Harlie was one", new Font(Font.TIMES_NEW_ROMAN, 12));
+            listItem.add(new Chunk(" by David Gerrold", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
+            list.add(listItem);
+            listItem = new ListItem("The World according to Garp", new Font(Font.TIMES_NEW_ROMAN, 12));
+            listItem.add(new Chunk(" by John Irving", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
+            list.add(listItem);
+            listItem = new ListItem("Decamerone", new Font(Font.TIMES_NEW_ROMAN, 12));
+            listItem.add(new Chunk(" by Giovanni Boccaccio", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
+            list.add(listItem);
+            document.add(list);
         }
         catch(DocumentException de) {
             System.err.println(de.getMessage());
@@ -73,6 +71,7 @@ public class Chap0803 {
         catch(IOException ioe) {
             System.err.println(ioe.getMessage());
         }
+        
         // step 5: we close the document
         document.close();
     }
