68403769ebe9f455b5803a18d20e4a81c4843aaa
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-01-09 14:03:55 +0000

    image masks

--- www/examples/Chap0112.java
+++ www/examples/Chap0112.java
@@ -1,3 +1,28 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2002 by Bruno Lowagie <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
 import java.io.*;
 
 import com.lowagie.text.*;
