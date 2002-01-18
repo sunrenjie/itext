f1207325fbb384aa7b268ccd70fccc4f2727d619
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-01-18 15:13:44 +0000

    pantones

--- www/examples/Chap1013.java
+++ www/examples/Chap1013.java
@@ -1,3 +1,28 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2002 by Phillip@formstar.com <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://www.lowagie.com/iText/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * ir-arch Bruno Lowagie,
+ * Adolf Baeyensstraat 121
+ * 9040 Sint-Amandsberg
+ * BELGIUM
+ * tel. +32 (0)9 228.10.97
+ * bruno@lowagie.com
+ */
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.awt.Color;
@@ -18,7 +43,7 @@ public class Pantone {
             // step 2:
             // we create a writer that listens to the document
             // and directs a PDF-stream to a file
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Pantone.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("Chap1013.pdf"));
             BaseFont bf = BaseFont.createFont("Helvetica", "winansi", BaseFont.NOT_EMBEDDED);
             
             // step 3: we open the document
