dc196b64312b44531f609baab75623f405557f69
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-03-01 11:20:59 +0000

    extra examples fonts

--- www/examples/Chap0906.java
+++ www/examples/Chap0906.java
@@ -5,7 +5,7 @@
  * This code is free software. It may only be copied or modified
  * if you include the following copyright notice:
  *
- * --> Copyright 2001 by Bruno Lowagie <--
+ * --> Copyright 2001 by Paulo Soares, Bruno Lowagie <--
  *
  * This code is part of the 'iText Tutorial'.
  * You can find the complete tutorial at the following address:
@@ -17,48 +17,55 @@
  *
  * itext@lowagie.com
  */
+
+import java.awt.Color;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+
 import com.lowagie.text.*;
-import com.lowagie.text.pdf.PdfWriter;
-import com.lowagie.text.pdf.PdfBarcode;
 import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfWriter;
+
 public class Chap0906 {
     
     public static void main(String[] args) {
         
-        System.out.println("Chapter 9 example 6: Barcodes with ttf");
+        System.out.println("Chapter 9 example 6: FontFactory");
+        
+        FontFactory.register("c:\\winnt\\fonts\\comicbd.ttf");
+        FontFactory.register("c:\\winnt\\fonts\\comic.ttf");
+        FontFactory.register("c:\\winnt\\fonts\\msgothic.ttc");
+        System.out.println("These fonts were registered at the FontFactory");
+        for (Iterator i = FontFactory.getRegisteredFonts().iterator(); i.hasNext(); ) {
+            System.out.println((String) i.next());
+        }
         
         // step 1: creation of a document-object
         Document document = new Document();
         
         try {
-            
             // step 2:
             // we create a writer that listens to the document
             // and directs a PDF-stream to a file
-            
             PdfWriter.getInstance(document, new FileOutputStream("Chap0906.pdf"));
             
             // step 3: we open the document
             document.open();
             
             // step 4: we add content to the document
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\CODE39.TTF", PdfBarcode.CODE39, 36, "0123456789")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\UPC-A.TTF", PdfBarcode.UPCA, 36, "203489343822")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\UPC-AHH.TTF", PdfBarcode.UPCA, 36, "203489343822")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\EAN-13.TTF", PdfBarcode.EAN13, 36, "8010012529736")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\EAN-13HH.TTF", PdfBarcode.EAN13, 48, "5400111151441")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\EAN-13B.TTF", PdfBarcode.EAN13, 60, "8010012529736")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\EAN-13BH.TTF", PdfBarcode.EAN13, 72, "5400111151441")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\I2OF5.TTF", PdfBarcode.INTERLEAVED_2_OF_5, 48, "12345678900987654321")));
-            document.add(new Paragraph(new PdfBarcode("c:\\winnt\\fonts\\I2OF5NT.TTF", PdfBarcode.INTERLEAVED_2_OF_5, 48, "2345678900987654321")));       
-            // measuring the length of a string
-            BaseFont bf = BaseFont.createFont("c:\\winnt\\fonts\\CODE39.TTF", BaseFont.CP1252, BaseFont.NOT_EMBEDDED);
-            float points = bf.getWidthPoint("0123456789", 36f);
-            float inches = points / 72f;
-            float cm = inches * 2.54f;
-            System.out.println("points: " + points + "; inches: " + inches + "; cm: " + cm);
+            Font font0 = FontFactory.getFont(BaseFont.HELVETICA, BaseFont.WINANSI, 12);
+            String text0 = "This is the quite popular built in font '" + BaseFont.HELVETICA + "'.";
+            document.add(new Paragraph(text0, font0));
+            Font font1 = FontFactory.getFont("ComicSansMS", BaseFont.WINANSI, 12);
+            String text1 = "This is the quite popular True Type font 'ComicSansMS'.";
+            document.add(new Paragraph(text1, font1));
+            Font font2 = FontFactory.getFont("ComicSansMS-Bold", BaseFont.WINANSI, 12);
+            String text2 = "This is the quite popular True Type font 'ComicSansMS-Bold'.";
+            document.add(new Paragraph(text2, font2));
+            Font font3= FontFactory.getFont("MS-PGothic", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, 12);
+            String text3 = "\u5951\u7d04\u8005\u4f4f\u6240\u30e9\u30a4\u30f3\uff11";
+            document.add(new Paragraph(text3, font3));
         }
         catch(DocumentException de) {
             System.err.println(de.getMessage());
