7aa32050b6529f46ffbdf5641ab4dd66b6103e02
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-03-04 16:03:12 +0000

    example keeping a paragraph together

--- www/examples/Chap0206.java
+++ www/examples/Chap0206.java
@@ -18,24 +18,21 @@
  * itext@lowagie.com
  */
 
-import java.awt.Color;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.TreeMap;
+import java.awt.Color;
 
 import com.lowagie.text.*;
 import com.lowagie.text.pdf.PdfWriter;
-import com.lowagie.text.pdf.PdfPageEventHelper;
 
-public class Chap0206 implements SplitCharacter {
+public class Chap0206 {
     
     public static void main(String[] args) {
         
-        System.out.println("Chapter 2 example 6: split character");
+        System.out.println("Chapter 2 example 6: keeping a paragraph together");
         
         // step 1: creation of a document-object
-        Document document = new Document();
+        Document document = new Document(PageSize.A6);
         
         try {
             
@@ -48,17 +45,21 @@ public class Chap0206 implements SplitCharacter {
             document.open();
             
             // step 4:
-            // we add some content
-            String text = "Some.text.to.show.the.splitting.action.of.the.interface.";
-            Chap0206 split = new Chap0206();
-            Chunk ck = new Chunk(text, new Font(Font.HELVETICA, 24));
-            Paragraph p = new Paragraph(24, ck);
-            document.add(new Paragraph("Normal split."));
+            Paragraph p;
+            p = new Paragraph("GALLIA est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.  Hi omnes lingua, institutis, legibus inter se differunt. Gallos ab Aquitanis Garumna flumen, a Belgis Matrona et Sequana dividit. Horum omnium fortissimi sunt Belgae, propterea quod a cultu atque humanitate provinciae longissime absunt, minimeque ad eos mercatores saepe commeant atque ea quae ad effeminandos animos pertinent important, proximique sunt Germanis, qui trans Rhenum incolunt, quibuscum continenter bellum gerunt.  Qua de causa Helvetii quoque reliquos Gallos virtute praecedunt, quod fere cotidianis proeliis cum Germanis contendunt, cum aut suis finibus eos prohibent aut ipsi in eorum finibus bellum gerunt.", new Font(Font.HELVETICA, 12));
+            p.setKeepTogether(true);
             document.add(p);
-            ck = new Chunk(text, new Font(Font.HELVETICA, 24));
-            ck.setSplitCharacter(split);
-            p = new Paragraph(24, ck);
-            document.add(new Paragraph("The dot '.' is the split character."));
+            p = new Paragraph("[Eorum una, pars, quam Gallos obtinere dictum est, initium capit a flumine Rhodano, continetur Garumna flumine, Oceano, finibus Belgarum, attingit etiam ab Sequanis et Helvetiis flumen Rhenum, vergit ad septentriones. Belgae ab extremis Galliae finibus oriuntur, pertinent ad inferiorem partem fluminis Rheni, spectant in septentrionem et orientem solem. Aquitania a Garumna flumine ad Pyrenaeos montes et eam partem Oceani quae est ad Hispaniam pertinet; spectat inter occasum solis et septentriones.]", new Font(Font.HELVETICA, 12));
+            p.setKeepTogether(true);
+            document.add(p);
+            p = new Paragraph("Apud Helvetios longe nobilissimus fuit et ditissimus Orgetorix.  Is M. Messala, [et P.] M.  Pisone consulibus regni cupiditate inductus coniurationem nobilitatis fecit et civitati persuasit ut de finibus suis cum omnibus copiis exirent:  perfacile esse, cum virtute omnibus praestarent, totius Galliae imperio potiri.  Id hoc facilius iis persuasit, quod undique loci natura Helvetii continentur:  una ex parte flumine Rheno latissimo atque altissimo, qui agrum Helvetium a Germanis dividit; altera ex parte monte Iura altissimo, qui est inter Sequanos et Helvetios; tertia lacu Lemanno et flumine Rhodano, qui provinciam nostram ab Helvetiis dividit.  His rebus fiebat ut et minus late vagarentur et minus facile finitimis bellum inferre possent; qua ex parte homines bellandi cupidi magno dolore adficiebantur.  Pro multitudine autem hominum et pro gloria belli atque fortitudinis angustos se fines habere arbitrabantur, qui in longitudinem milia passuum CCXL, in latitudinem CLXXX patebant.", new Font(Font.HELVETICA, 12));
+            p.setKeepTogether(true);
+            document.add(p);
+            p = new Paragraph("His rebus adducti et auctoritate Orgetorigis permoti constituerunt ea quae ad proficiscendum pertinerent comparare, iumentorum et carrorum quam maximum numerum coemere, sementes quam maximas facere, ut in itinere copia frumenti suppeteret, cum proximis civitatibus pacem et amicitiam confirmare.  Ad eas res conficiendas biennium sibi satis esse duxerunt; in tertium annum profectionem lege confirmant.  Ad eas res conficiendas Orgetorix deligitur.  Is sibi legationem ad civitates suscipit.  In eo itinere persuadet Castico, Catamantaloedis filio, Sequano, cuius pater regnum in Sequanis multos annos obtinuerat et a senatu populi Romani amicus appellatus erat, ut regnum in civitate sua occuparet, quod pater ante habuerit; itemque Dumnorigi Haeduo, fratri Diviciaci, qui eo tempore principatum in civitate obtinebat ac maxime plebi acceptus erat, ut idem conaretur persuadet eique filiam suam in matrimonium dat.  Perfacile factu esse illis probat conata perficere, propterea quod ipse suae civitatis imperium obtenturus esset:  non esse dubium quin totius Galliae plurimum Helvetii possent; se suis copiis suoque exercitu illis regna conciliaturum confirmat.  Hac oratione adducti inter se fidem et ius iurandum dant et regno occupato per tres potentissimos ac firmissimos populos totius Galliae sese potiri posse sperant.", new Font(Font.HELVETICA, 12));
+            p.setKeepTogether(true);
+            document.add(p);
+            p = new Paragraph("Ea res est Helvetiis per indicium enuntiata.  Moribus suis Orgetoricem ex vinculis causam dicere coegerunt; damnatum poenam sequi oportebat, ut igni cremaretur.  Die constituta causae dictionis Orgetorix ad iudicium omnem suam familiam, ad hominum milia decem, undique coegit, et omnes clientes obaeratosque suos, quorum magnum numerum habebat, eodem conduxit; per eos ne causam diceret se eripuit.  Cum civitas ob eam rem incitata armis ius suum exequi conaretur multitudinemque hominum ex agris magistratus cogerent, Orgetorix mortuus est; neque abest suspicio, ut Helvetii arbitrantur, quin ipse sibi mortem consciverit.", new Font(Font.HELVETICA, 12));
+            p.setKeepTogether(true);
             document.add(p);
             
         }
@@ -71,14 +72,5 @@ public class Chap0206 implements SplitCharacter {
         
         // step 5: we close the document
         document.close();
-    }
-    
-    /**
-     * Returns <CODE>true</CODE> if the character can split a line.
-     * @param c the character
-     * @return <CODE>true</CODE> if the character can split a line
-     */
-    public boolean isSplitCharacter(char c) {
-        return (c == '.');
     }
 }
