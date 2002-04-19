9a6b74491dbd8a790bb785c4adb2c017aa825497
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2002-04-19 10:11:23 +0000

    changed a jarname

--- trunk/www/examples/compile.bat
+++ trunk/www/examples/compile.bat
@@ -1,2 +1,2 @@
 javac -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/xerces_1_3_1.jar;. %1.java
-java -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/itext-hyph.jar;../../bin/xerces_1_3_1.jar;. %1
+java -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/iTextHYPH.jar;../../bin/xerces_1_3_1.jar;. %1
