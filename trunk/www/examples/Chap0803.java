f91339708249df2f990edb41986fe869c2631c1d
hallm <hallm@3a709846-db2a-4fbd-be0b-eb932eee6479>
2003-02-25 13:51:45 +0000

    Changed the example to demonstrate the RtfHeaderFooters class

--- trunk/www/examples/Chap0803.java
+++ trunk/www/examples/Chap0803.java
@@ -23,51 +23,72 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.lowagie.text.*;
-import com.lowagie.text.rtf.RtfWriter;
+import com.lowagie.text.rtf.*;
 
+/**
+ * This example creates a RTF document with more complex headers and footers
+ * using the RtfHeaderFooters class.
+ *
+ * @author <a href="mailto:mhall@myrealbox.com">Mark.Hall@myrealbox.com</a>
+ */
 public class Chap0803 {
 
     public static void main(String[] args) {
 
-        System.out.println("Chapter 8 example 3: Lists and RTF");
+        System.out.println("Chapter 8 example 3: RTF with the RtfHeaderFooters class");
 
-        // step 1: creation of a document-object
-        Document document = new Document();
+        /* Create a new document */
+        Document document = new Document(PageSize.A4);
+        try
+            {
+            /* Create a RtfWriter and a PdfWriter for the document */
+            RtfWriter rtf = RtfWriter.getInstance(document, new FileOutputStream("Chap0803.rtf"));
 
-        try {
+            /* We specify that the RTF file has a Title Page */
+            rtf.setHasTitlePage(true);
 
-            // step 2:
-            // we create a writer that listens to the document
-            // and directs a PDF-stream to a file
-            RtfWriter.getInstance(document, new FileOutputStream("Chap0803.rtf"));
+            /* We create headers and footers for the RTF file */
+            RtfHeaderFooters header = new RtfHeaderFooters();
+            RtfHeaderFooters footer = new RtfHeaderFooters();
 
-            // step 3: we open the document
+            /* We add a header that will only appear on the first page */
+            header.set(RtfHeaderFooters.FIRST_PAGE, new HeaderFooter(new Phrase("This header is only on the first page"), false));
+            /* We add a header that will only appear on left-side pages */
+            header.set(RtfHeaderFooters.LEFT_PAGES, new HeaderFooter(new Phrase("This header is only on left pages"), false));
+            /* We add a header that will only appear on right-side pages */
+            header.set(RtfHeaderFooters.RIGHT_PAGES, new HeaderFooter(new Phrase("This header is only on right pages. "), false));
+            /* We add a footer that will appear on all pages except the first (because of the title page)
+               Because the header has different left and right page footers, we have to add the footer to
+               both the left and right pages. */
+            footer.set(RtfHeaderFooters.LEFT_PAGES, new HeaderFooter(new Phrase("This footer is on all pages except the first. Page: "), true));
+            footer.set(RtfHeaderFooters.RIGHT_PAGES, new HeaderFooter(new Phrase("This footer is on all pages except the first. Page: "), true));
+
+            /* Open the document */
             document.open();
 
-            // step 4:
 
-            document.add(new Paragraph("some books I really like:"));
-            ListItem listItem;
-            List list = new List(true, 15);
-            listItem = new ListItem("When Harlie was one", new Font(Font.TIMES_NEW_ROMAN, 12));
-            listItem.add(new Chunk(" by David Gerrold", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
-            list.add(listItem);
-            listItem = new ListItem("The World according to Garp", new Font(Font.TIMES_NEW_ROMAN, 12));
-            listItem.add(new Chunk(" by John Irving", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
-            list.add(listItem);
-            listItem = new ListItem("Decamerone", new Font(Font.TIMES_NEW_ROMAN, 12));
-            listItem.add(new Chunk(" by Giovanni Boccaccio", new Font(Font.TIMES_NEW_ROMAN, 11, Font.ITALIC)));
-            list.add(listItem);
-            document.add(list);
-        }
-        catch(DocumentException de) {
-            System.err.println(de.getMessage());
-        }
-        catch(IOException ioe) {
-            System.err.println(ioe.getMessage());
-        }
-        
-        // step 5: we close the document
+            /* We add the header and footer */
+            document.setHeader(header);
+            document.setFooter(footer);
+
+
+            /* We add some content */
+            Chapter chapter = new Chapter(new Paragraph("Advanced RTF headers and footers", new Font(Font.HELVETICA, 16, Font.BOLD)), 1);
+
+            chapter.add(new Paragraph("This document demonstrates the use of advanced RTF headers and footers."));
+
+            for(int i = 0; i < 300; i++)
+                {
+                chapter.add(new Paragraph("Line " + i));
+                }
+            document.add(chapter);
+            }
+        catch(DocumentException de)
+            { System.err.println(de.getMessage()); }
+        catch(IOException ioe)
+            { System.err.println(ioe.getMessage()); }
+
+        /* Close the document */
         document.close();
     }
 }
