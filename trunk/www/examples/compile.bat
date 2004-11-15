3871b8802b0218528f29f98784f5f91cc000d630
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2004-11-15 09:46:31 +0000

    cleanup except for the examples by Mark Hall

--- trunk/www/examples/compile.bat
+++
@@ -1,2 +0,0 @@
-javac -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;%J2EE_HOME%/lib/j2ee.jar;. %1.java
-java -classpath ../../bin/iText.jar;../../bin/iTextXML.jar;../../bin/iTextHYPH.jar;../../bin/iTextAsian.jar;../../bin/iTextAsianCmaps.jar;%JAVA_HOME%/jre/lib/ext/jai_core.jar;%JAVA_HOME%/jre/lib/ext/jai_codec.jar;%J2EE_HOME%/lib/j2ee.jar;. %1
