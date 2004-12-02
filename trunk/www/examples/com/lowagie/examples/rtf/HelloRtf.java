28a3362d9272c8acbd353319ee0531fd1bf2dc94
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2004-12-02 11:47:24 +0000

    moving RTF examples

---
+++ trunk/www/examples/com/lowagie/examples/rtf/HelloRtf.java
@@ -0,0 +1,71 @@
+/*
+ * $Id$
+ * $Name$
+ *
+ * This code is free software. It may only be copied or modified
+ * if you include the following copyright notice:
+ *
+ * --> Copyright 2001 by Bruno Lowagie <--
+ * --> Copyright 2004 by Mark Hall <--
+ *
+ * This code is part of the 'iText Tutorial'.
+ * You can find the complete tutorial at the following address:
+ * http://itext.sourceforge.net/tutorial/
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * itext-questions@lists.sourceforge.net
+ */
+
+package com.lowagie.examples.rtf;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.rtf.RtfWriter2;
+
+public class HelloRtf {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World example in RTF");
+        
+        // step 1: creation of a document-object
+        Document document = new Document();
+        
+        try {
+            
+            // step 2:
+            // we create a writer that listens to the document
+            // and directs a RTF-stream to a file
+            
+            RtfWriter2.getInstance(document, new FileOutputStream("HelloWorld.rtf"));
+            
+            // step 3: we open the document
+            document.open();
+            
+            // step 4: we add a paragraph to the document
+			Paragraph p = new Paragraph("Hello");
+			for (int i = 0; i < args.length; i++) {
+				p.add(" ");
+				p.add(args[i]);
+			}
+            document.add(p);
+			
+            
+        }
+        catch(DocumentException de) {
+            System.err.println(de.getMessage());
+        }
+        catch(IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+        
+        // step 5: we close the document
+        document.close();
+    }
+}
