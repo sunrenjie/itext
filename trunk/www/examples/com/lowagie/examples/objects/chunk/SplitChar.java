15943b853bb301b9f2100f935f5c585f4ac06973
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2004-12-21 13:15:42 +0000

    new examples

--- trunk/www/examples/com/lowagie/examples/objects/chunk/SplitChar.java
+++ trunk/www/examples/com/lowagie/examples/objects/chunk/SplitChar.java
@@ -5,7 +5,7 @@
  * This code is free software. It may only be copied or modified
  * if you include the following copyright notice:
  *
- * --> Copyright 2001-2004 by Bruno Lowagie <--
+ * --> Copyright 2001-2004 by Paulo Soares <--
  *
  * This code is part of the 'iText Tutorial'.
  * You can find the complete tutorial at the following address:
@@ -20,33 +20,35 @@
 
 package com.lowagie.examples.objects.chunk;
 
-import java.awt.Color;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.SplitCharacter;
+import com.lowagie.text.pdf.PdfChunk;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
- * Demonstrates how 'highlight' Chunks using a Background color.
+ * Demonstrates the use of the splitcharacter.
  * 
  * @author blowagie
  */
 
-public class Background {
+public class SplitChar implements SplitCharacter {
 
 	/**
-	 * Using Background Colors in Chunks.
+	 * Demonstrates the use of the splitcharacter.
 	 * 
 	 * @param args no arguments needed here
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Background color");
+		System.out.println("SplitCharacter");
 
 		// step 1: creation of a document-object
 		Document document = new Document();
@@ -54,37 +56,26 @@ public class Background {
 			// step 2:
 			// we create a writer that listens to the document
 			PdfWriter.getInstance(document,
-					new FileOutputStream("Background.pdf"));
+					new FileOutputStream("SplitChar.pdf"));
 
 			// step 3: we open the document
 			document.open();
 			// step 4:
-			Chunk high = new Chunk("highlighted");
-			high.setBackground(new Color(0xFF, 0xFF, 0x00));
-			Paragraph p = new Paragraph("The following chunk is ");
-			p.add(high);
-			document.add(p);
-			Chunk c;
-			c = new Chunk("background");
-			c.setBackground(new Color(0xC0, 0xC0, 0xC0));
-			document.add(c);
-			c = new Chunk("background");
-			c.setTextRise(8);
-			c.setBackground(new Color(0xFF, 0xDE, 0xAD));
-			document.add(c);
-			c = new Chunk("background", FontFactory.getFont(FontFactory.HELVETICA, 8));
-			c.setBackground(new Color(0x70, 0x70, 0x70));
-			document.add(c);
-			c = new Chunk("background");
-			c.setBackground(new Color(0x00, 0x80, 0x80));
-			document.add(c);
-			document.add(Chunk.NEWLINE);
-			p = new Paragraph("An more complex ");
-			c = new Chunk("background chunk");
-			c.setBackground(new Color(0xFF, 0x00, 0x00), 5f, 30f, -10f, 0f);
-			p.add(c);
-			p.add(" example");
-			document.add(p);
+			Chunk chunk;
+			Paragraph p;
+            String text = "Some.text.to.show.the.splitting.action.of.the.interface.";
+            Font font = FontFactory.getFont(FontFactory.HELVETICA, 24);
+            
+            document.add(new Paragraph("Normal split."));
+            chunk = new Chunk(text, font);
+            p = new Paragraph(24, chunk);
+            document.add(p);
+            
+            document.add(new Paragraph("The dot '.' is the split character."));
+            chunk = new Chunk(text, font);
+            chunk.setSplitCharacter(new SplitChar());
+            p = new Paragraph(24, chunk);
+            document.add(p);
 		} catch (DocumentException de) {
 			System.err.println(de.getMessage());
 		} catch (IOException ioe) {
@@ -93,5 +84,17 @@ public class Background {
 
 		// step 5: we close the document
 		document.close();
+	}
+
+	/**
+	 * @see com.lowagie.text.SplitCharacter#isSplitCharacter(int, int, int, char[], com.lowagie.text.pdf.PdfChunk[])
+	 */
+	public boolean isSplitCharacter(int start, int current, int end, char[] cc, PdfChunk[] ck) {
+        char c;
+        if (ck == null)
+            c = cc[current];
+        else
+            c = ck[Math.min(current, ck.length - 1)].getUnicodeEquivalent(cc[current]);
+        return (c == '.');
 	}
 }
