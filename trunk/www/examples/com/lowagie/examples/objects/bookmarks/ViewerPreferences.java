230289e16bacc0266c524a9b1a2d3c8c5d0b0ba6
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2005-01-05 08:35:50 +0000

    new examples + moving examples to new package

--- trunk/www/examples/com/lowagie/examples/objects/bookmarks/ViewerPreferences.java
+++ trunk/www/examples/com/lowagie/examples/objects/bookmarks/ViewerPreferences.java
@@ -18,7 +18,7 @@
  * itext-questions@lists.sourceforge.net
  */
 
-package com.lowagie.examples.objects.anchors;
+package com.lowagie.examples.objects.bookmarks;
 
 import java.io.FileOutputStream;
 
@@ -26,10 +26,6 @@ import com.lowagie.text.Document;
 import com.lowagie.text.Font;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.PdfContentByte;
-import com.lowagie.text.pdf.PdfDestination;
-import com.lowagie.text.pdf.PdfOutline;
-import com.lowagie.text.pdf.PdfPageEventHelper;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
@@ -38,23 +34,7 @@ import com.lowagie.text.pdf.PdfWriter;
  * @author blowagie
  */
 
-public class ViewerPreferences extends PdfPageEventHelper {
-    
-    /** Keeps the number of the current paragraph. */
-    private int n = 0;
-    
-    /**
-     * Adds an outline for every new Paragraph
-     * @param writer
-     * @param document
-     * @param position
-     */
-    public void onParagraph(PdfWriter writer, Document document, float position) {
-        n++;
-        PdfContentByte cb = writer.getDirectContent();
-        PdfDestination destination = new PdfDestination(PdfDestination.FITH, position);
-        PdfOutline outline = new PdfOutline(cb.getRootOutline(), destination, "paragraph " + n);
-    }
+public class ViewerPreferences {
 
 	/**
 	 * Creates documents with different viewerpreferences.
@@ -76,14 +56,10 @@ public class ViewerPreferences extends PdfPageEventHelper {
 			PdfWriter writer3 = PdfWriter.getInstance(document, new FileOutputStream("FullScreen.pdf"));
 			// step 3:
             writer1.setViewerPreferences(PdfWriter.PageLayoutTwoColumnLeft);
-            writer2.setViewerPreferences(PdfWriter.PageModeUseOutlines | PdfWriter.HideMenubar | PdfWriter.HideToolbar);
-            writer3.setViewerPreferences(PdfWriter.PageLayoutTwoColumnRight | PdfWriter.PageModeFullScreen | PdfWriter.NonFullScreenPageModeUseOutlines);
+            writer2.setViewerPreferences(PdfWriter.HideMenubar | PdfWriter.HideToolbar);
+            writer3.setViewerPreferences(PdfWriter.PageLayoutTwoColumnRight | PdfWriter.PageModeFullScreen | PdfWriter.NonFullScreenPageModeUseThumbs);
 			document.open();
             // step 4: we grab the ContentByte and do some stuff with it
-            writer1.setPageEvent(new Bookmarks());
-            writer2.setPageEvent(new Bookmarks());
-            writer3.setPageEvent(new Bookmarks());
-            
             document.add(new Paragraph("GALLIA est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.  Hi omnes lingua, institutis, legibus inter se differunt. Gallos ab Aquitanis Garumna flumen, a Belgis Matrona et Sequana dividit. Horum omnium fortissimi sunt Belgae, propterea quod a cultu atque humanitate provinciae longissime absunt, minimeque ad eos mercatores saepe commeant atque ea quae ad effeminandos animos pertinent important, proximique sunt Germanis, qui trans Rhenum incolunt, quibuscum continenter bellum gerunt.  Qua de causa Helvetii quoque reliquos Gallos virtute praecedunt, quod fere cotidianis proeliis cum Germanis contendunt, cum aut suis finibus eos prohibent aut ipsi in eorum finibus bellum gerunt.", new Font(Font.HELVETICA, 12)));
             document.add(new Paragraph("[Eorum una, pars, quam Gallos obtinere dictum est, initium capit a flumine Rhodano, continetur Garumna flumine, Oceano, finibus Belgarum, attingit etiam ab Sequanis et Helvetiis flumen Rhenum, vergit ad septentriones. Belgae ab extremis Galliae finibus oriuntur, pertinent ad inferiorem partem fluminis Rheni, spectant in septentrionem et orientem solem. Aquitania a Garumna flumine ad Pyrenaeos montes et eam partem Oceani quae est ad Hispaniam pertinet; spectat inter occasum solis et septentriones.]", new Font(Font.HELVETICA, 12)));
             document.add(new Paragraph("Apud Helvetios longe nobilissimus fuit et ditissimus Orgetorix.  Is M. Messala, [et P.] M.  Pisone consulibus regni cupiditate inductus coniurationem nobilitatis fecit et civitati persuasit ut de finibus suis cum omnibus copiis exirent:  perfacile esse, cum virtute omnibus praestarent, totius Galliae imperio potiri.  Id hoc facilius iis persuasit, quod undique loci natura Helvetii continentur:  una ex parte flumine Rheno latissimo atque altissimo, qui agrum Helvetium a Germanis dividit; altera ex parte monte Iura altissimo, qui est inter Sequanos et Helvetios; tertia lacu Lemanno et flumine Rhodano, qui provinciam nostram ab Helvetiis dividit.  His rebus fiebat ut et minus late vagarentur et minus facile finitimis bellum inferre possent; qua ex parte homines bellandi cupidi magno dolore adficiebantur.  Pro multitudine autem hominum et pro gloria belli atque fortitudinis angustos se fines habere arbitrabantur, qui in longitudinem milia passuum CCXL, in latitudinem CLXXX patebant.", new Font(Font.HELVETICA, 12)));
