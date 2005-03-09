506533a499f03ffefae82febacdd799000cdf1c6
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2005-03-09 10:12:46 +0000

    some changes

--- trunk/www/examples/com/lowagie/examples/objects/tables/alternatives/TableWithImage.java
+++ trunk/www/examples/com/lowagie/examples/objects/tables/alternatives/TableWithImage.java
@@ -22,24 +22,26 @@ package com.lowagie.examples.objects.tables.alternatives;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.lowagie.text.Cell;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Image;
 import com.lowagie.text.Table;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
  * A very simple Table example.
  */
-public class MyFirstTable {
+public class TableWithImage {
 
 	/**
-	 * A very simple Table example.
+	 * A very simple PdfPTable example.
 	 * 
 	 * @param args
 	 *            no arguments needed
 	 */
 	public static void main(String[] args) {
-		System.out.println("My first table");
+		System.out.println("A table with Image");
 		// step 1: creation of a document-object
 		Document document = new Document();
 		try {
@@ -47,15 +49,19 @@ public class MyFirstTable {
 			// we create a writer that listens to the document
 			// and directs a PDF-stream to a file
 			PdfWriter.getInstance(document,
-					new FileOutputStream("MyFirstTable.pdf"));
+					new FileOutputStream("imageTable.pdf"));
 			// step 3: we open the document
 			document.open();
 			// step 4: we create a table and add it to the document
 			Table table = new Table(2, 2); // 2 rows, 2 columns
-			table.addCell("0.0");
-			table.addCell("0.1");
-			table.addCell("1.0");
-			table.addCell("1.1");
+			table.addCell(new Cell(Image.getInstance("otsoe.jpg")));
+			table.addCell(new Cell(Image.getInstance("iText.gif")));
+			Cell c1 = new Cell();
+			c1.add(Image.getInstance("iText.gif"));
+			table.addCell(c1);
+			Cell c2 = new Cell();
+			c2.add(Image.getInstance("otsoe.jpg"));
+			table.addCell(c2);
 			document.add(table);
 		} catch (DocumentException de) {
 			System.err.println(de.getMessage());
