79ffe100672e2699d4f118118cb9963e17443fdf
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2005-03-31 14:16:21 +0000

    colors

--- www/examples/com/lowagie/examples/directcontent/colors/Groups.java
+++ www/examples/com/lowagie/examples/directcontent/colors/Groups.java
@@ -23,11 +23,8 @@ import java.awt.Color;
 import java.io.FileOutputStream;
 
 import com.lowagie.text.Document;
-import com.lowagie.text.Element;
 import com.lowagie.text.PageSize;
-import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.ColumnText;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfGState;
 import com.lowagie.text.pdf.PdfTemplate;
@@ -37,7 +34,7 @@ import com.lowagie.text.pdf.PdfWriter;
 /**
  * Demonstrates transparency and images.
  */
-public class Transparency {
+public class Groups {
     
     /**
      * Prints a square and fills half of it with a gray rectangle.
@@ -48,7 +45,7 @@ public class Transparency {
      */
     public static void pictureBackdrop(float x, float y, PdfContentByte cb) throws Exception {
         cb.setColorStroke(Color.black);
-        cb.setColorFill(Color.gray);
+        cb.setColorFill(Color.red);
         cb.rectangle(x, y, 100, 200);
         cb.fill();
         cb.setLineWidth(2);
@@ -64,13 +61,15 @@ public class Transparency {
      * @throws Exception
      */
     public static void pictureCircles(float x, float y, PdfContentByte cb) throws Exception {
-        cb.setColorFill(Color.red);
+    	PdfGState gs = new PdfGState();
+    	gs.setBlendMode(PdfGState.BM_SOFTLIGHT);
+    	gs.setFillOpacity(0.7f);
+    	cb.setGState(gs);
+        cb.setColorFill(Color.gray);
         cb.circle(x + 70, y + 70, 50);
         cb.fill();
-        cb.setColorFill(Color.yellow);
         cb.circle(x + 100, y + 130, 50);
         cb.fill();
-        cb.setColorFill(Color.blue);
         cb.circle(x + 130, y + 70, 50);
         cb.fill();
     }
@@ -80,12 +79,12 @@ public class Transparency {
      * @param args no arguments needed
      */
     public static void main(String[] args) {
-		System.out.println("Transparency");
+		System.out.println("Transparency Groups");
         // step 1: creation of a document-object
         Document document = new Document(PageSize.A4, 50, 50, 50, 50);
         try {
             // step 2: creation of a writer 
-            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("transparency.pdf"));
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("groups.pdf"));
             // step 3: we open the document
             document.open();
             // step 4: content
@@ -97,52 +96,45 @@ public class Transparency {
             pictureBackdrop(200 + 2 * gap, 500, cb);
             pictureBackdrop(gap, 500 - 200 - gap, cb);
             pictureBackdrop(200 + 2 * gap, 500 - 200 - gap, cb);
+
+            PdfTemplate tp;
+            PdfTransparencyGroup group;
             
-            pictureCircles(gap, 500, cb);
-            cb.saveState();
-            PdfGState gs1 = new PdfGState();
-            gs1.setFillOpacity(0.5f);
-            cb.setGState(gs1);
-            pictureCircles(200 + 2 * gap, 500, cb);
-            cb.restoreState();
-
-            PdfTemplate tp = cb.createTemplate(200, 200);
-            cb.saveState();
-            pictureCircles(0, 0, tp);
-            PdfTransparencyGroup group = new PdfTransparencyGroup();
-            tp.setGroup(group);
-            cb.setGState(gs1);
-            cb.addTemplate(tp, gap, 500 - 200 - gap);
-            cb.restoreState();
-
             tp = cb.createTemplate(200, 200);
-            cb.saveState();
-            PdfGState gs2 = new PdfGState();
-            gs2.setFillOpacity(0.5f);
-            gs2.setBlendMode(PdfGState.BM_SOFTLIGHT);
-            tp.setGState(gs2);
             pictureCircles(0, 0, tp);
+            group = new PdfTransparencyGroup();
+            group.setIsolated(true);
+            group.setKnockout(true);
+            tp.setGroup(group);
+            cb.addTemplate(tp, gap, 500);
+
+            
+            tp = cb.createTemplate(200, 200);
+            pictureCircles(0, 0, tp);
+            group = new PdfTransparencyGroup();
+            group.setIsolated(true);
+            group.setKnockout(false);
+            tp.setGroup(group);
+            cb.addTemplate(tp, 200 + 2 * gap, 500);
+
+            
+            tp = cb.createTemplate(200, 200);
+            pictureCircles(0, 0, tp);
+            group = new PdfTransparencyGroup();
+            group.setIsolated(false);
+            group.setKnockout(true);
+            tp.setGroup(group);
+            cb.addTemplate(tp, gap, 500 - 200 - gap);
+
+            
+            tp = cb.createTemplate(200, 200);
+            pictureCircles(0, 0, tp);
+            group = new PdfTransparencyGroup();
+            group.setIsolated(false);
+            group.setKnockout(false);
             tp.setGroup(group);
             cb.addTemplate(tp, 200 + 2 * gap, 500 - 200 - gap);
-            cb.restoreState();
 
-            cb.resetRGBColorFill();
-            ColumnText ct = new ColumnText(cb);
-            Phrase ph = new Phrase("Ungrouped objects\nObject opacity = 1.0");
-            ct.setSimpleColumn(ph, gap, 0, gap + 200, 500, 18, Element.ALIGN_CENTER);
-            ct.go();
-            
-            ph = new Phrase("Ungrouped objects\nObject opacity = 0.5");
-            ct.setSimpleColumn(ph, 200 + 2 * gap, 0, 200 + 2 * gap + 200, 500, 18, Element.ALIGN_CENTER);
-            ct.go();
-            
-            ph = new Phrase("Transparency group\nObject opacity = 1.0\nGroup opacity = 0.5\nBlend mode = Normal");
-            ct.setSimpleColumn(ph, gap, 0, gap + 200, 500 - 200 - gap, 18, Element.ALIGN_CENTER);
-            ct.go();
-            
-            ph = new Phrase("Transparency group\nObject opacity = 0.5\nGroup opacity = 1.0\nBlend mode = SoftLight");
-            ct.setSimpleColumn(ph, 200 + 2 * gap, 0, 200 + 2 * gap + 200, 500 - 200 - gap, 18, Element.ALIGN_CENTER);
-            ct.go();
         }
         catch (Exception de) {
             de.printStackTrace();
