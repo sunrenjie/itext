674e9cf9ed6de0b2e661490f0f05804b9fe7e472
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2005-04-03 07:56:53 +0000

    rearranging object examples

--- trunk/www/examples/com/lowagie/examples/objects/NegativeLeading.java
+++ trunk/www/examples/com/lowagie/examples/objects/NegativeLeading.java
@@ -18,7 +18,7 @@
  * itext-questions@lists.sourceforge.net
  */
 
-package com.lowagie.examples.objects.chunk;
+package com.lowagie.examples.objects;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -29,38 +29,39 @@ import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfWriter;
 
 /**
- * Shows how special characters are substituted with Phrase.getInstance.
+ * Shows the effect of a negative leading.
  * 
  * @author blowagie
  */
 
-public class SymbolSubstitution {
+public class NegativeLeading {
 
 	/**
-	 * How to substiture special characters with Phrase.getInstance.
+	 * Demonstrates what happens if you choose a negative leading.
 	 * 
-	 * @param args no arguments needed here
+	 * @param args
+	 *            no arguments needed here
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Symbol Substitution");
+		System.out.println("Negative Leading");
 
 		// step 1: creation of a document-object
 		Document document = new Document();
 		try {
 			// step 2:
 			// we create a writer that listens to the document
-			PdfWriter.getInstance(document,
-					new FileOutputStream("SymbolSubstitution.pdf"));
+			PdfWriter.getInstance(document, new FileOutputStream(
+					"NegativeLeading.pdf"));
 
 			// step 3: we open the document
 			document.open();
 			// step 4:
-            document.add(Phrase.getInstance("What is the " + (char) 945 + "-coefficient of the "
-                    + (char) 946 + "-factor in the " + (char) 947 + "-equation?\n"));
-                    for (int i = 913; i < 970; i++) {
-                        document.add(Phrase.getInstance(" " + String.valueOf(i) + ": " + (char) i));
-                    }
+			document.add(new Phrase(16, "\n\n\n"));
+			document
+					.add(new Phrase(
+							-16,
+							"Hello, this is a very long phrase to show you the somewhat odd effect of a negative leading. You can write from bottom to top. This is not fully supported. It's something between a feature and a bug."));
 		} catch (DocumentException de) {
 			System.err.println(de.getMessage());
 		} catch (IOException ioe) {
