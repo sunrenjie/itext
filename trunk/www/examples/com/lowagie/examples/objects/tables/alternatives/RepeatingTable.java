0ee9fb0a5c9349470b609baf9de4e51b162e4900
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2007-11-21 15:24:27 +0000

    update deprecation tags changed set/getDefaultLayout in Table to set/getDefaultCell removal of some deprecated code that should have been removed long ago

--- trunk/www/examples/com/lowagie/examples/objects/tables/alternatives/RepeatingTable.java
+++ trunk/www/examples/com/lowagie/examples/objects/tables/alternatives/RepeatingTable.java
@@ -70,8 +70,8 @@ public class RepeatingTable {
             datatable.addCell(cell);
             
             // These cells span 2 rows
-            datatable.getDefaultLayout().setBorderWidth(2);
-            datatable.getDefaultLayout().setHorizontalAlignment(1);
+            datatable.getDefaultCell().setBorderWidth(2);
+            datatable.getDefaultCell().setHorizontalAlignment(1);
             datatable.addCell("User Id");
             datatable.addCell("Name\nAddress");
             datatable.addCell("Company");
@@ -86,18 +86,18 @@ public class RepeatingTable {
             // this is the end of the table header
             datatable.endHeaders();
             
-            datatable.getDefaultLayout().setBorderWidth(1);
+            datatable.getDefaultCell().setBorderWidth(1);
             
             for (int i = 1; i < 30; i++) {
                 
-                datatable.getDefaultLayout().setHorizontalAlignment(Element.ALIGN_LEFT);
+                datatable.getDefaultCell().setHorizontalAlignment(Element.ALIGN_LEFT);
                 
                 datatable.addCell("myUserId");
                 datatable.addCell("Somebody with a very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very, very long long name");
                 datatable.addCell("No Name Company");
                 datatable.addCell("D" + i);
                 
-                datatable.getDefaultLayout().setHorizontalAlignment(Element.ALIGN_CENTER);
+                datatable.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
                 datatable.addCell("No");
                 datatable.addCell("Yes");
                 datatable.addCell("No");
