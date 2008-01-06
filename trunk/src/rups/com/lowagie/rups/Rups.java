8b5fc9945775c688b80fd478aa63caa86f2c539a
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2008-01-06 14:41:31 +0000

    Caching icons RUPS version information MessageAction to show messages About and Versions items in Help menu

--- trunk/src/rups/com/lowagie/rups/Rups.java
+++ trunk/src/rups/com/lowagie/rups/Rups.java
@@ -34,6 +34,9 @@ import com.lowagie.rups.controller.RupsController;
  */
 public class Rups {
 
+	/** The version String; this String may only be changed by Bruno Lowagie. */
+	public static String VERSION = "RUPS 0.0.1 (by lowagie.com)";
+	
 	// main method
 	/**
 	 * Main method of this PdfFrame class.
