d617abcb193819d1051c9f124e85afdbc4d7f839
eugenemark <eugenemark@3a709846-db2a-4fbd-be0b-eb932eee6479>
2013-02-15 15:09:05 +0000

    DEV-487 issue with PDF/A-1B compliance,digital signature and keywords

--- pdfa/src/main/java/com/itextpdf/text/pdf/PdfAStamperImp.java
+++ pdfa/src/main/java/com/itextpdf/text/pdf/PdfAStamperImp.java
@@ -116,9 +116,9 @@ public class PdfAStamperImp extends PdfStamperImp {
     }
 
     /**
-     * @see PdfStamperImp#getXmpWriter(java.io.ByteArrayOutputStream, com.itextpdf.text.pdf.PdfDocument.PdfInfo)
+     * @see PdfStamperImp#getXmpWriter(java.io.ByteArrayOutputStream, com.itextpdf.text.pdf.PdfDictionary)
      */
-    protected XmpWriter getXmpWriter(ByteArrayOutputStream baos, PdfDocument.PdfInfo info) throws IOException {
+    protected XmpWriter getXmpWriter(ByteArrayOutputStream baos, PdfDictionary info) throws IOException {
         if (xmpWriter == null)
             xmpWriter = new PdfAXmpWriter(baos, info, ((PdfAConformance)pdfIsoConformance).getConformanceLevel());
         return xmpWriter;
