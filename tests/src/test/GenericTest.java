3e6ef8a0faf64a7b2c1b1c151c96ea359d50f474
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2014-01-24 15:07:46 +0000

    Cleaning up (part 1)

--- tests/src/test/GenericTest.java
+++ tests/src/test/GenericTest.java
@@ -16,16 +16,29 @@ import com.itextpdf.text.log.LoggerFactory;
 
 public abstract class GenericTest {
 	
+	/** The logger class */
     private final static Logger LOGGER = LoggerFactory.getLogger(GenericTest.class.getName());
 
-	protected Class<?> klass;
+    /** The class file for the example we're going to test. */
+	private Class<?> klass;
+	/** An error message */
     private String errorMessage;
+    /** A prefix that is part of the error message. */
     private String differenceImagePrefix = "difference";
 
+    /**
+     * Gets triggered before the test is performed.
+     * When writing tests, you need to override this method to set
+     * the klass variable (using the setKlass() method)
+     */
     @Before
     public void setup() {
     }
 
+    /**
+     * Creates a Class object for the example you want to test.
+     * @param	className	the class you want to test
+     */
 	protected void setKlass(String className) {
 		try {
 			klass = Class.forName(className);
@@ -34,40 +47,71 @@ public abstract class GenericTest {
 		}
 	}
 
+	/**
+	 * Tests the example.
+	 * If SRC and DEST are defined, the example manipulates a PDF;
+	 * if only DEST is defined, the example creates a PDF.
+	 */
     @Test(timeout = 60000)
     public void test() throws Exception {
         LOGGER.info("Starting test.");
+        // Getting the destination PDF file (must be there!)
         String dest= getDest();
         if (dest == null || dest.length() == 0)
             throw new OperationsException("outPdf cannot be empty!");
+        // Getting the source PDF file
         String src = getSrc();
+        // if there is none, just create a PDF
         if (src == null || src.length() == 0) {
         	createPdf(src);
         }
+        // if there is one, manipulate the PDF
         else {
         	manipulatePdf(src, dest);
         }
+        // Do some further tests on the PDF
         assertPdf(dest);
+        // Compare the destination PDF with a reference PDF
         comparePdf(dest, getCmpPdf());
         LOGGER.info("Test complete.");
     }
     
+    /**
+     * Creates a PDF by invoking the createPdf() method in the
+     * original sample class.
+     * @param	dest	the resulting PDF
+     */
 	protected void createPdf(String dest) throws Exception {
         LOGGER.info("Creating PDF.");
     	Method method = klass.getDeclaredMethod("createPdf", String.class);
     	method.invoke(klass.getConstructor().newInstance(), dest);
 	}
     
+    /**
+     * Manupulates a PDF by invoking the manipulatePdf() method in the
+     * original sample class.
+     * @param	src		the source PDF
+     * @param	dest	the resulting PDF
+     */
 	protected void manipulatePdf(String src, String dest) throws Exception {
         LOGGER.info("Manipulating PDF.");
     	Method method = klass.getDeclaredMethod("manipulatePdf", String.class, String.class);
     	method.invoke(klass.getConstructor().newInstance(), src, dest);
 	}
 	
+	/**
+	 * Gets the path to the source PDF from the sample class.
+	 * @return	a path to a source PDF
+	 */
 	protected String getSrc() {
 		return getStringField("SRC");
 	}
 	
+	/**
+	 * Gets the path to the resulting PDF from the sample class;
+	 * this method also creates directories if necessary.
+	 * @return	a path to a resulting PDF
+	 */
 	protected String getDest() {
 		String dest = getStringField("DEST");
 		if (dest != null) {
@@ -77,6 +121,12 @@ public abstract class GenericTest {
 		return dest;
 	}
 	
+	/**
+	 * Returns a string value that is stored as a static variable
+	 * inside an example class.
+	 * @param name	the name of the variable
+	 * @return	the value of the variable
+	 */
 	protected String getStringField(String name) {
 		try {
 			Field field = klass.getField(name);
@@ -92,22 +142,38 @@ public abstract class GenericTest {
 		}
 	}
 
-    protected void comparePdf(String outPdf, String cmpPdf) throws Exception {
-    	if (cmpPdf == null || cmpPdf.length() == 0) return;
-        CompareTool compareTool = new CompareTool(outPdf, cmpPdf);
-        String outPath = "./target/" + new File(outPdf).getParent();
+	/**
+	 * Compares two PDF files using iText's CompareTool.
+	 * @param	dest	the PDF that resulted from the test
+	 * @param	cmp		the reference PDF
+	 */
+    protected void comparePdf(String dest, String cmp) throws Exception {
+    	if (cmp == null || cmp.length() == 0) return;
+        CompareTool compareTool = new CompareTool(dest, cmp);
+        String outPath = "./target/" + new File(dest).getParent();
         new File(outPath).mkdirs();
-        addError(compareTool.compare(outPdf, cmpPdf, outPath, differenceImagePrefix));
-        addError(compareTool.compareDocumentInfo(outPdf, cmpPdf));
-        addError(compareTool.compareLinks(outPdf, cmpPdf));
+        addError(compareTool.compare(dest, cmp, outPath, differenceImagePrefix));
+        addError(compareTool.compareDocumentInfo(dest, cmp));
+        addError(compareTool.compareLinks(dest, cmp));
 
         if (errorMessage != null) Assert.fail(errorMessage);
     }
 	
-    protected void assertPdf(String outPdf) throws Exception {};
+    /**
+     * Perform other tests on the resulting PDF.
+     * @param	dest	the resulting PDF
+     */
+    protected void assertPdf(String dest) throws Exception {};
 
-    protected String getCmpPdf() { return ""; }
+    /**
+     * Every test needs to know where to find its reference file.
+     */
+    protected abstract String getCmpPdf();
 
+    /**
+     * Helper method to construct error messages.
+     * @param	error	part of an error message.
+     */
     private void addError(String error) {
         if (error != null && error.length() > 0) {
             if (errorMessage == null)
@@ -116,30 +182,6 @@ public abstract class GenericTest {
                 errorMessage += "\n";
 
             errorMessage += error;
-        }
-    }
-
-    protected void deleteDirectory(File path) {
-        if (path == null)
-            return;
-        if (path.exists()) {
-            for (File f : path.listFiles()) {
-                if (f.isDirectory()) {
-                    deleteDirectory(f);
-                    f.delete();
-                } else {
-                    f.delete();
-                }
-            }
-            path.delete();
-        }
-    }
-
-    protected void deleteFiles(File path) {
-        if (path != null && path.exists()) {
-            for (File f : path.listFiles()) {
-                f.delete();
-            }
         }
     }
 }
--- tests/src/test/images/LargeImage1.java
+++ tests/src/test/images/LargeImage1.java
@@ -1,63 +1,16 @@
 package test.images;
 
-import java.io.FileOutputStream;
+import test.GenericTest;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PRStream;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfName;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.parser.PdfImageObject;
-import test.SandboxTest;
+public class LargeImage1 extends GenericTest {
 
-public class LargeImage1 extends SandboxTest {
-
-    String inputPdf = "./resources/pdfs/large_image.pdf";
-
-    @Override
-    protected String getOutPdf() {
-        return "./results/images/large_image1.pdf";
-    }
+	@Override
+	public void setup() {
+		setKlass("sandbox.images.LargeImage1");
+	}
 
     @Override
     protected String getCmpPdf() {
-        return "./resources/results/images/cmp_large_image1.pdf";
-    }
-
-    @Override
-    public void makePdf(String outPdf) throws Exception {
-        PdfReader reader = new PdfReader(inputPdf);
-        // The width and the height of a PDF page may not exceed 14400 user units:
-        Rectangle rect = reader.getPageSize(1);
-        if (rect.getWidth() < 14400 && rect.getHeight() < 14400) {
-            System.out.println("The size of the PDF document is withing the accepted limits");
-            System.exit(0);
-        }
-        // We assume that there's a single large picture on the first page
-        PdfDictionary page = reader.getPageN(1);
-        PdfDictionary resources = page.getAsDict(PdfName.RESOURCES);
-        PdfDictionary xobjects = resources.getAsDict(PdfName.XOBJECT);
-        PdfName imgRef = xobjects.getKeys().iterator().next();
-        PRStream imgStream = (PRStream) xobjects.getAsStream(imgRef);
-        // We now create a new Image object based on the bytes in the stream
-        PdfImageObject imgObject = new PdfImageObject(imgStream);
-        Image img = Image.getInstance(imgObject.getImageAsBytes());
-        img.scaleToFit(14400, 14400);
-        img.setAbsolutePosition(0, 0);
-        reader.close();
-        // We create a new document with the correct size
-        Document document = new Document(new Rectangle(img.getScaledWidth(), img.getScaledHeight()));
-        PdfWriter.getInstance(document, new FileOutputStream(outPdf));
-        document.open();
-        document.add(img);
-        document.close();
-    }
-
-    public static void main(String[] args) throws Exception {
-        SandboxTest test = new LargeImage1();
-        test.makePdf();
+        return "./results/images/cmp_large_image1.pdf";
     }
 }
