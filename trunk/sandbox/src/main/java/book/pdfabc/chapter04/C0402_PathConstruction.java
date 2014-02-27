236b305589286e92522b6167b3a59c34bbd3ec52
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2014-02-27 09:21:32 +0000

    Adding a comment section to the examples. Renaming chapter 4 examples to reflect correct chapter number Turning the chapter 4 examples into a test

--- trunk/sandbox/src/main/java/book/pdfabc/chapter04/C0402_PathConstruction.java
+++ trunk/sandbox/src/main/java/book/pdfabc/chapter04/C0402_PathConstruction.java
@@ -1,22 +1,30 @@
+/**
+ * This is an example was written by Bruno Lowagie.
+ * It is used in the book "The ABC of PDF".
+ * This book can be downloaded here: https://leanpub.com/itext_pdfabc/
+ */
 package book.pdfabc.chapter04;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import sandbox.WrapToTest;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class C0202_PathConstruction {
+@WrapToTest
+public class C0402_PathConstruction {
 
-    public static final String DEST = "results/pdfabc/c04/path_construction.pdf";
+    public static final String DEST = "results/pdfabc/chapter04/path_construction.pdf";
     
     public static void main(String[] args) throws IOException, DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new C0202_PathConstruction().createPdf(DEST);
+        new C0402_PathConstruction().createPdf(DEST);
     }
     
     public void createPdf(String dest) throws IOException, DocumentException {
