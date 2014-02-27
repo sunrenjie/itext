62f0aefc3e0026adc8bd317621fdd6ff8340c5d3
blowagie <blowagie@3a709846-db2a-4fbd-be0b-eb932eee6479>
2014-02-27 09:21:32 +0000

    Adding a comment section to the examples. Renaming chapter 4 examples to reflect correct chapter number Turning the chapter 4 examples into a test

--- sandbox/src/main/java/book/pdfabc/chapter04/C0401_ImagingModel.java
+++ sandbox/src/main/java/book/pdfabc/chapter04/C0401_ImagingModel.java
@@ -1,8 +1,15 @@
+/**
+ * This is an example was written by Bruno Lowagie.
+ * It is used in the book "The ABC of PDF".
+ * This book can be downloaded here: https://leanpub.com/itext_pdfabc/
+ */
 package book.pdfabc.chapter04;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import sandbox.WrapToTest;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
@@ -10,14 +17,15 @@ import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
 
-public class C0201_ImagingModel {
+@WrapToTest
+public class C0401_ImagingModel {
 
-    public static final String DEST = "results/pdfabc/c04/hello_world.pdf";
+    public static final String DEST = "results/pdfabc/chapter04/hello_world.pdf";
     
     public static void main(String[] args) throws IOException, DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new C0201_ImagingModel().createPdf(DEST);
+        new C0401_ImagingModel().createPdf(DEST);
     }
     
     public void createPdf(String dest) throws IOException, DocumentException {
@@ -30,17 +38,17 @@ public class C0201_ImagingModel {
         // step 4
         PdfContentByte canvas = writer.getDirectContent();
         // THIS IS NOT THE USUAL WAY TO ADD TEXT; THIS IS THE HARD WAY!!!
-        canvas.beginText();
-        canvas.moveText(36, 788);
-        canvas.setFontAndSize(BaseFont.createFont(), 12);
-        canvas.showText("Hello World ");
-        canvas.endText();
+        canvas.beginText();                               // BT
+        canvas.moveText(36, 788);                         // 36 788 Td
+        canvas.setFontAndSize(BaseFont.createFont(), 12); // /F1 12 Tf
+        canvas.showText("Hello World ");                  // (Hello World )Tj
+        canvas.endText();                                 // ET
         // This creates a path for a line and strokes it
-        canvas.saveState();
-        canvas.moveTo(0, 0);
-        canvas.lineTo(595, 842);
-        canvas.stroke();
-        canvas.restoreState();
+        canvas.saveState();      // q
+        canvas.moveTo(0, 0);     // 0 0 m
+        canvas.lineTo(595, 842); // 595 842 l
+        canvas.stroke();         // S
+        canvas.restoreState();   // Q
         // step 5
         document.close();
     }
